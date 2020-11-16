25b34dc605cefc51ddb42ab79df98526a80c6543
SUJcreate <66529938+SUJcreate@users.noreply.github.com>
2020-11-16 15:31:26 +0530

    testing

--- Backend/models/Buffet.js
+++ Backend/models/Buffet.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
-<<<<<<< Updated upstream
 const buffetSchema=new mongoose.Schema({
   
   
@@ -45,35 +44,6 @@ const buffetSchema=new mongoose.Schema({
 
   }]}]
 
-=======
-const buffetSchema = new mongoose.Schema({
-  restaurant_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'Restaurant',
-    required: true
-  },
-  slots: [{
-    time: {
-      starting: {
-        type: Date,
-        required: true
-      },
-      ending: {
-        type: Date,
-        required: true
-      }
-    },
-    isAvailable: {
-      type: Boolean,
-      default: true
-    },
-    bookedBy: [{
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
-    }],
-  }]
->>>>>>> Stashed changes
 });
 const Buffet = mongoose.model('Buffet', buffetSchema);
 module.exports = Buffet;
