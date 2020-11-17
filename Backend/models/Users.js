361281db95761783d695a565ae9808e779a258ac
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2020-11-17 16:30:44 +0530

    main.js

--- Backend/models/Users.js
+++ Backend/models/Users.js
@@ -29,6 +29,17 @@ const userSchema = new mongoose.Schema({
     type: String,
     enum: ['Customer', 'Reception', 'Manager', 'Restaurant Owner']
   },
+  password: {
+    type: String,
+    minlength: 6,
+    validate: {
+      validator: (v) => {
+        return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,1024}$/.test(v);
+      },
+      message: "weak password"
+    },
+    required: [true, "Password Required"]
+  },
   email: {
     type: String,
     validate: {
@@ -38,8 +49,8 @@ const userSchema = new mongoose.Schema({
     },
     required: [true, "Email required"]
   },
-  document: {
-    type: String
+  verification: {
+    type: Schema.Types.Mixed
   },
   isVerified: {
     default: false,
--- src/main.js
+++ src/main.js
@@ -2,31 +2,34 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import {
+  BootstrapVue,
+  IconsPlugin
+} from 'bootstrap-vue'
 import './assets/app.scss'
 import 'bootstrap';
 import * as firebase from "firebase/app";
 import "firebase/auth";
 var firebaseConfig = {
-  apiKey: "AIzaSyChXM9NITNI4lW9qnLxIPFSpk-GE6P58Es",
-  authDomain: "nuclues-project.firebaseapp.com",
-  databaseURL: "https://nuclues-project.firebaseio.com",
-  projectId: "nuclues-project",
-  storageBucket: "nuclues-project.appspot.com",
-  messagingSenderId: "358452549767",
-  appId: "1:358452549767:web:8f22a51ba6d314d5334022"
+  apiKey: "AIzaSyB4fI0N4OgXzEXdsioGfAbu_xZvAM7xO_Q",
+  authDomain: "resort1234.firebaseapp.com",
+  databaseURL: "https://resort1234.firebaseio.com",
+  projectId: "resort1234",
+  storageBucket: "resort1234.appspot.com",
+  messagingSenderId: "764353730728",
+  appId: "1:764353730728:web:b83f84b68722e1feba4a9d",
+  measurementId: "G-03W0BMCB8N"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.auth().onAuthStateChanged(user=>{
-  store.commit("updateUser",{user});
+firebase.auth().onAuthStateChanged(user => {
+  store.commit("updateUser", {
+    user
+  });
 });
 Vue.use(BootstrapVue)
-
 Vue.use(IconsPlugin)
-
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
