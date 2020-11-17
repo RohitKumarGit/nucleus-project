c125283ff8c54baf1fbe563d0476483e431393e5
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2020-11-17 13:13:00 +0530

    Updated buffet

--- Backend/routers/buffet.js
+++ Backend/routers/buffet.js
@@ -23,5 +23,21 @@ router.get('/buffet', async (req, res) => {
   }
 });
 router.patch('/buffet', async (req, res) => {
-
+  try {
+    var restaurant = await Restaurant.findOne({
+      name: req.body.name
+    });
+    if (!restaurant) {
+      throw new Error();
+    }
+    var id = restaurant._id;
+    var buffet = await Buffet.findOne({
+      restaurant_id: id
+    });
+    //Enter Your Code Here!
+  } catch (e) {
+    res.status(500).send({
+      error: "Some problem with updates"
+    });
+  }
 });
