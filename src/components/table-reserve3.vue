c9a49f31a0a73d357642654e928e6910a3710b4b
mayank6725 <69050987+mayank6725@users.noreply.github.com>
2020-11-23 21:16:14 +0530

    made food menu and food order

--- src/components/table-reserve3.vue
+++ src/components/table-reserve3.vue
@@ -29,12 +29,12 @@
             Welcome to Desi Addicts
           </h3>
           <form class="form-box px-3">
-            <a href="#popup" class="btn btn-secondary" id="food-menu-button">Show Food Menu</a>
+            <a href="#popup1" class="btn btn-secondary" id="food-menu-button">Show Food Menu</a>
             <h6 class="ques">Do You want to pre-order food as well?</h6>
             <label>&nbsp;&nbsp;<input type="radio" name="optradio" v-model="x" value="yes">&nbsp;&nbsp;Yes</label>&nbsp;&nbsp;&nbsp;&nbsp;
             <label>&nbsp;&nbsp;<input type="radio" name="optradio" v-model="x" value="no">&nbsp;&nbsp;No</label>&nbsp;&nbsp;&nbsp;&nbsp;
             <br>
-            <button type="button" v-show="x==='yes'" class="btn btn-secondary">Select Food items</button>
+            <a href="#popup2" class="btn btn-secondary" v-if="x=='yes'">Select Food items</a>
             <div class="mb-3">
               <button type="submit" class="btn btn-block text-uppercase">
                 Reserve Table
@@ -48,8 +48,9 @@
  </div>
  </div>
  </div>
- <div class="popup" id="popup">
-   <div class="popup__content">
+
+  <div class="popup" id="popup1">
+   <div class="popup__content menu-popup">
     <div class="container my-container">
       <div class="row my-row">
         <div class="col-lg-6 d-none d-lg-block my-col left-popup">
@@ -68,6 +69,60 @@
     </div>
    </div>
  </div>
+
+ <div class="popup" id="popup2">
+   <div class="popup__content food-order">
+    <div class="food-order-heading">
+      <h1>ORDER FOOD</h1>
+    </div>
+    
+    <div class="container food-order-container">
+      <div class="row food-order-pane">
+        <div class="col-lg-6 col-md-12 u-add-height u-made-scroll">
+        <h3 class="food-order-pane-heading">FOOD ITEMS</h3>
+        
+          <h4 class="food-order-types">MAIN COURSE</h4>
+          <ul class="food-order-items">
+            <li class="custom">DAL MAKHANI<div class="hover-text">-->dal makhani is a veg food</div></li><b-form-spinbutton class="my-spin-button" min=0></b-form-spinbutton>
+            <li class="custom">CHANA MASALA<div class="hover-text">-->chana masala is a veg food</div></li><b-form-spinbutton class="my-spin-button" min=0></b-form-spinbutton>
+            <li class="custom">PANEER TIKKA BUTTER MASALA</li><b-form-spinbutton class="my-spin-button" min=0></b-form-spinbutton>
+            <li class="custom">PALAK PANEER</li><b-form-spinbutton class="my-spin-button" min=0></b-form-spinbutton>
+            <li class="custom">MUSHROOM MASALA</li><b-form-spinbutton class="my-spin-button" min=0></b-form-spinbutton>
+          </ul>
+          <h4 class="food-order-types">SALADS</h4>
+          <ul class="food-order-items">
+            <li class="custom">Vegetable Som Tam Salad</li><b-form-spinbutton class="my-spin-button" min=0></b-form-spinbutton>
+            <li class="custom">Compressed Melon Sliders</li><b-form-spinbutton class="my-spin-button" min=0></b-form-spinbutton>
+            <li class="custom">Mixed Beans Salad</li><b-form-spinbutton class="my-spin-button" min=0></b-form-spinbutton>
+            <li class="custom">Fruit Cube Salad</li><b-form-spinbutton class="my-spin-button" min=0></b-form-spinbutton>
+          </ul>
+          <h4 class="food-order-types">DRINKS</h4>
+          <ul class="food-order-items">
+            <li class="custom">vodka lemon</li><b-form-spinbutton class="my-spin-button" min=0></b-form-spinbutton>
+            <li class="custom">coke</li><b-form-spinbutton class="my-spin-button" min=0></b-form-spinbutton>
+            <li class="custom">orange juice</li><b-form-spinbutton class="my-spin-button" min=0></b-form-spinbutton>
+          </ul>
+          <h4 class="food-order-types">DESSERTS</h4>
+          <ul class="food-order-items">
+            <li class="custom">choco-lave cake</li><b-form-spinbutton class="my-spin-button" min=0></b-form-spinbutton>
+            <li class="custom">ras-malai</li><b-form-spinbutton class="my-spin-button" min=0></b-form-spinbutton>
+            <li class="custom">rice kheer</li><b-form-spinbutton class="my-spin-button" min=0></b-form-spinbutton>
+          </ul>
+      </div>
+        <div class="col-lg-6 col-md-12 u-add-height u-made-scroll">
+          <h3 class="food-order-pane-heading">CART</h3>
+          <div class="food-order-cart"><h4 class="food-order-types">TOTAL</h4>        
+          </div>
+          <button class="btn btn--blue order-now-btn">order now&rarr;</button>
+      </div>
+      </div>
+    </div>
+   </div>
+ </div>
+
+ 
+
+
  </div>
  </div>
 </template>
@@ -245,6 +300,7 @@ img{
     opacity: 0;
     visibility: hidden;
     transition: all .3s;
+    overflow-y:auto;   
 
     &__content {
         @include absCenter;
@@ -258,8 +314,7 @@ img{
         opacity: 0;
         transform: translate(-50%, -50%) scale(.25);
         transition: all .5s .2s;
-          background-image:linear-gradient(to right bottom,#cc2b5e ,#753a88);
-          padding:0.5rem;
+        overflow-y:auto;   
     }
 
     &__text {
@@ -351,5 +406,185 @@ height:100%;
 }
 .menu-card-content{
   margin-left:1rem;
+}
+.menu-popup
+{
+  background-image:linear-gradient(to right bottom,#cc2b5e ,#753a88);
+          padding:0.5rem;
+}
+
+.food-order
+{
+  background-image:url(../assets/foodorder.jpg);
+  background-size:cover;
+  filter: blur(0.1px);
+  &-heading
+ {
+  color:white;
+  text-shadow: 1px 1px #fe4902, 
+                2px 2px #fe4902, 
+                3px 3px #fe4902;
+  width:28%;
+  margin:1rem auto;
+  &:hover {
+   position: relative; 
+   top: -3px; 
+   left: -3px; 
+   text-shadow: 1px 1px #fe4902, 
+                2px 2px #fe4902, 
+                3px 3px #fe4902, 
+                4px 4px #fe4902, 
+                5px 5px #fe4902, 
+                6px 6px #fe4902;
+}
+ }
+ &-pane
+ {
+  &-heading
+  {
+    color:#2a2a2a;
+    text-shadow: 1px 1px #fe4902, 
+                2px 2px #fe4902, 
+                3px 3px #fe4902;
+    font-size:2.2rem;
+
+  }
+ }
+ &-container
+ {
+  opacity:0.85;
+  border-radius:5px;
+ }
+ &-items{
+  text-transform:uppercase;
+  list-style:square outside none;
+
+ }
+ &-types{
+  font-family: 'Ubuntu', sans-serif;
+  font-weight: bold;
+  text-shadow: 2px 7px 5px rgba(0,0,0,0.3), 
+    0px -4px 10px rgba(255,255,255,0.3);
+ }
+}
+.u-add-height{
+  height:30rem;
+}
+.u-made-scroll{
+  overflow-y:auto;
+}
+.u-made-scroll::-webkit-scrollbar {
+    width: 12px;
+}
+
+.u-made-scroll::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+    border-radius: 10px;
+}
+
+.u-made-scroll::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5); 
+}
+.custom
+{
+  font-weight:bold;
+  font-style:italic;
+  line-height:2;
+  &:hover>.hover-text{
+   display:block;
+   position:absolute;
+   left:10rem;
+  }
+}
+.my-spin-button{
+  width:20%;
+  font-weight:bold;
+  background-color:lightgrey;
+  height:2rem;
+}
+.hover-text
+{
+  display:none;
+  font-size:0.8rem;
+  width:50%;
+}
+
+.btn {
+    &,
+    &:link,
+    &:visited {
+        text-transform: uppercase;
+        text-decoration: none;
+        padding: 1rem 3.5rem;
+        display: inline-block;
+        border-radius: 10rem;
+        transition: all .2s;
+        position: relative;
+
+        border: none;
+        cursor: pointer;
+    }
+    
+    &:hover {
+        transform: translateY(-3px);
+        box-shadow: 0 1rem 2rem rgba($color-black,.2);
+        color:white;
+
+        &::after {
+            transform: scaleX(1.4) scaleY(1.6);
+            opacity: 0;
+        }
+    }
+    
+    &:active,
+    &:focus {
+        outline: none;
+        transform: translateY(-1px);
+        box-shadow: 0 .5rem 1rem rgba($color-black,.2);
+    }
+    
+    &--white {
+        background-color: $color-white;
+        color: $color-grey-dark;
+
+        &::after {
+            background-color: $color-white;
+        }
+    }
+
+    &--blue {
+        background-color: #FF9900;
+        margin-top:1rem;
+        font-size:1rem;
+        font-weight:bold;
+        color:black;
+        &::after {
+            background-color: #FF9900;
+        }
+    }
+    
+    &::after {
+        content: "";
+        display: inline-block;
+        height: 100%;
+        width: 100%;
+        border-radius: 10rem;
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        transition: all .4s;
+    }
+    
+    &--animated {
+        animation: moveInBottom .5s ease-out .75s;
+        animation-fill-mode: backwards;
+    }
+}
+.order-now-btn{
+  position:absolute;
+  bottom:1rem;
+  left:10rem;
 }
 </style>
