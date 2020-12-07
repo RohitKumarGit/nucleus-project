13de8414e9361fad266269280c38e21a7d3f7a7f
RohitKumarGit <59276987+RohitKumarGit@users.noreply.github.com>
2020-12-07 09:53:04 +0530

    psp

--- Backend/models/Buffet.js
+++ Backend/models/Buffet.js
@@ -1,63 +1,24 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 const buffetSchema = new mongoose.Schema({
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
   restaurant_id: {
     type: Schema.Types.ObjectId,
     ref: 'Restaurant',
     required: true
   },
-  slots: [{
-    slot_type: {
-      type: String,
-      required: true,
-      enum: ['Morning Mania', 'Lovely Lunch', 'Delightful Dinner']
-    },
-    slot_details: [{
-      time: {
-        type: Number,
-        required: true
-      },
-      Limit: {
-        type: Number,
-        required: true,
-        min: 1
-      },
-      totalPeople: {
-        type: Number,
-        required: true,
-        default: 0,
-      },
-      isAvailable: {
-        type: Boolean,
-        default: true
-      },
-      bookedBy: [{
-        user_id: {
-          type: Schema.Types.ObjectId,
-          ref: 'User',
-          required: true
-        },
-        number: {
-          type: Number,
-          required: true,
-          default: 1
-        }
-      }],
-    }]
-  }]
-
+  slot_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'Slot',
+    required: true
+  },
+  people: {
+    type: Number
+  }
 });
-var schedule = require('node-schedule');
-
-var rule = new schedule.RecurrenceRule();
-rule.hour = 0 // hour 0-23
-rule.minute = 0 // minute 0 - 59
-var j = schedule.scheduleJob(rule, async function () {
-  // do the reset here @Sujal :) Thanks - done
-  await Buffet.updateMany({}, {
-    'slots.slot_details.isAvailable': true
-  })
-})
-
 const Buffet = mongoose.model('Buffet', buffetSchema);
 module.exports = Buffet;
--- Backend/routers/buffet.js
+++ Backend/routers/buffet.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Buffet = require('../models/Buffet');
+const Slot = require('../models/BuffetSlot');
 const Billing = require('../models/Billing');
 const Order = require('../models/Order')
 const Restaurant = require('../models/Restaurant');
@@ -15,23 +16,18 @@ router.get('/allbookings', async function (req, res) {
   console.log(user._id)
   try {
     const buffets = await Buffet.find({
-      'slots.slot_details.bookedBy.user_id': {
-        $in: [user._id]
-      }
-    }, "restaurant_id slots.slot_type slots.slot_details.time createdAt").populate('restaurant_id')
-   
+      user_id: user._id
+    }).populate('restaurant_id');
     const tables = await TableR.find({
       'user_id': {
         $in: [user._id]
       }
-    },{
-      isCancelled:false
     }).populate('restaurant_id')
     const orders = await Order.find({
-      "user_id":{
-        $in:[user._id]
+      "user_id": {
+        $in: [user._id]
       }
-    }).populate("restaurant_id")
+    }).populate("restaurant_id", "name")
     res.send({
       buffets,
       tables,
@@ -44,28 +40,18 @@ router.get('/allbookings', async function (req, res) {
     })
   }
 })
+
 router.get('/buffet', firebase.verifyToken, async (req, res) => {
   try {
-
     var restaurant = await Restaurant.findOne({
       name: req.query.name
     });
-
-    if (!restaurant) {
-      throw new Error();
-    }
-    var id = restaurant._id;
-    console.log(id)
-    var buffet = await Buffet.findOne({
-      restaurant_id: id
+    var slots = await Slot.find({
+      restaurant_id: restaurant._id
     });
-    console.log(buffet)
-    res.send(buffet);
+    res.send(slots);
   } catch (error) {
-    console.log(error)
-    res.send({
-      error: "Cannot GET"
-    });
+    res.send(error);
   }
 });
 
@@ -77,100 +63,57 @@ router.post('/buffet', firebase.verifyToken, async (req, res) => {
     var restaurant = await Restaurant.findOne({
       name: req.body.name
     });
-    if (!restaurant) {
-      throw new Error();
-    }
-    var id = restaurant._id;
-    var buffet = await Buffet.findOne({
-      restaurant_id: id
+    var slot = await Slot.findOne({
+      restaurant_id: restaurant._id,
+      slotType: req.body.slotType,
+      time: req.body.slotTime
     });
-    //0 for 1st slot 1 for 2nd and 2 for 3rd --> slotTime
-    var buffetType = buffet.slots[req.body.slotType].slot_details[req.body.slotTime];
-
-    if (buffetType.isAvailable) {
-      var x = req.body.number;
-      var y = buffetType.totalPeople;
-      if (Number(y) + Number(x) <= Number(buffetType.Limit)) {
-
-        buffetType.bookedBy.push({
-          user_id: user._id,
-          number: req.body.number
-        });
-        if (Number(y) + Number(x) === Number(buffetType.Limit)) {
-          buffetType.isAvailable = false;
-        }
-        buffetType.totalPeople += Number(x);
-        await buffet.save();
-        // user.forDashboard.buffet.push(buffet._id);
-        // await user.save();
-      }
-      var billing = new Billing({
-        user_id: user._id,
-        buffet_id: buffet._id,
-        totalBill: Number(req.body.number) * 500
-      });
-      await billing.save();
-      res.send(buffet);
-    } else {
-      throw new Error('Slot Full');
-    }
-  } catch (e) {
-    console.log(e);;
+    var people = req.body.number;
+    slot.currentCapacity += people;
+    var buffet = new Buffet({
+      user_id: user._id,
+      restaurant_id: restaurant._id,
+      slot_id: slot._id,
+      people: req.body.number
+    });
+    var billing = new Billing({
+      user_id: user._id,
+      buffet_id: buffet._id,
+      totalBill: Number(req.body.number) * 500
+    });
+    await billing.save();
+    await buffet.save();
+    await slot.save();
+    res.send(buffet);
+  } catch (error) {
+    res.send(error);
   }
 });
 
 router.post('/buffetcancel', firebase.verifyToken, async (req, res) => {
   try {
-    console.log(req.body)
-    var user = await User.findOne({
+    var user = User.findOne({
       uid: req.body.uid
     });
-    var user_id = user._id
-    console.log(user_id);
+    var buffetid = req.body.bid;
     var buffet = await Buffet.findOne({
-      _id: req.body.bid
+      _id: buffetid
     });
-    var buffetSlot = buffet.slots[req.body.slotType]; //Index 0-> MorningMania 1->Lunch 2->Dinner
-    var flag = false;
-    for (var i = 0; i < buffetSlot.slot_details.length; i++) {
-      var details = buffetSlot.slot_details[i];
-      if (details.time === req.body.time) {
-        var userIdx = -1;
-        for (var j = 0; j < details.bookedBy.length; j++) {
-          console.log(details.bookedBy[j].user_id);
-          if (String(user_id) == String(details.bookedBy[j].user_id)) {
-            userIdx = j;
-            break;
-          }
-        }
-        console.log(userIdx);
-        if (Number(userIdx) !== -1) {
-          var userNumber = details.bookedBy[userIdx].number;
-          console.log(userNumber);
-          details.totalPeople -= userNumber;
-          details.bookedBy.splice(userIdx, 1);
-          console.log(details.bookedBy);
-          flag = true;
-        }
-      }
-      if (flag) {
-        break;
-      }
-    }
-    // var index = user.forDashboard.buffet.indexOf(req.body.bid);
-    // if (index != -1) {
-    // user.forDashboard.buffet.splice(index, 1);
-    // }
+    var people = buffet.people;
+    var slotid = buffet.slot_id;
+    var slot = await Slot.findOne({
+      _id: slotid
+    });
+    slot.currentCapacity -= people;
     var billing = await Billing.findOne({
       user_id: user._id,
-      buffet_id: req.body.bid
+      buffet_id: buffetid,
     });
     await billing.remove();
-    // await user.save();
-    await buffet.save();
-    res.send(buffet);
-  } catch (e) {
-    res.send(e);
+    await slot.save();
+    await buffet.remove();
+  } catch (error) {
+    res.send(error);
   }
 });
 module.exports = router;
